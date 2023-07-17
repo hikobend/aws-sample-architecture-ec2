d2606a32bda45c5ccb59d69ede8544f8d25ddf37
hikobend <hiko.bkend@gmail.com>
2023-07-17 18:02:14 +0900

    ApplyのCIを走らせてリソースが作成されるか確認

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -22,3 +22,28 @@ module "s3_bucket" {
     name = "preprare-architecture-s3-bucket"
   }
 }
+
+module "delete_me_s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "delete-me-preprare-architecture-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+  versioning = {
+    enabled = true
+  }
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    name = "delete-me-preprare-architecture-s3-bucket"
+  }
+}
