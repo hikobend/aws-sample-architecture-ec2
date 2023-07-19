fc30ddee0123f05db952468b83eb6100fa2a50b2
hikobend <hiko.bkend@gmail.com>
2023-07-19 23:52:47 +0900

    設定値を環境変数

--- modules/dev_tfstate/main.tf
+++ modules/dev_tfstate/main.tf
@@ -2,7 +2,7 @@ module "s3_bucket_dev_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "dev-bucket-architecture-s3-bucket"
-  acl    = "private"
+  acl    = var.acl
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
@@ -13,7 +13,7 @@ module "s3_bucket_dev_bucket" {
   server_side_encryption_configuration = {
     rule = {
       apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
+        sse_algorithm = var.sse_algorithm
       }
     }
   }
@@ -27,13 +27,13 @@ module "dynamodb_table" {
   source                         = "terraform-aws-modules/dynamodb-table/aws"
   name                           = "set_dev_dynamo_db"
   billing_mode                   = "PAY_PER_REQUEST"
-  hash_key                       = "LockID"
+  hash_key                       = var.hash_key
   server_side_encryption_enabled = true
   point_in_time_recovery_enabled = true
 
   attributes = [
     {
-      name = "LockID"
+      name = var.hash_key
       type = "S"
     }
   ]
