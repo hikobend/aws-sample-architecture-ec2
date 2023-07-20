88a44ba622ce57e2216ba4017e813a63ab8ab4f8
hikobend <hiko.bkend@gmail.com>
2023-07-20 10:07:40 +0900

    vpc削除

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,6 +2,7 @@
 #   cidr_block = "10.0.0.0/24"
 # }
 
+
 module "dev_tfstate" { # tfstateを管理するリソースを作成 S3, DynamoDB
   source        = "../../modules/dev_tfstate"
   acl           = local.dev_tfstate_s3.acl
