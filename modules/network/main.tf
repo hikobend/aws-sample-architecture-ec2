c3b108a034d6315429a5e9793d2130e19085cec3
hikobend <hiko.bkend@gmail.com>
2023-07-23 21:45:15 +0900

    endpoint作成

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -100,23 +100,13 @@ module "database_sg" {
   ]
 }
 
-resource "aws_vpc_endpoint" "private_ec2_ssm" {
-  for_each = local.common_endpoint_config
-
-  vpc_id              = module.network.vpc_id
-  service_name        = each.value.service_name
-  vpc_endpoint_type   = "Interface"
-  private_dns_enabled = true
-  security_group_ids  = [module.ssm_sg.security_group_id]
-  subnet_ids          = [module.network.private_subnets[0], module.network.private_subnets[1]]
-
-  tags = {
-    Name = "${var.env}-${each.value.tag_name}",
-  }
-}
-
-locals {
-  common_endpoint_config = {
+variable "common_endpoint_config" {
+  description = "Common configuration for VPC endpoints"
+  type = map(object({
+    service_name = string
+    tag_name     = string
+  }))
+  default = {
     ssm = {
       service_name = "com.amazonaws.ap-northeast-1.ssm"
       tag_name     = "ssm"
@@ -131,3 +121,19 @@ locals {
     }
   }
 }
+
+resource "aws_vpc_endpoint" "private_ec2_ssm" {
+  for_each = var.common_endpoint_config
+
+  vpc_id              = module.network.vpc_id
+  service_name        = each.value.service_name
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  security_group_ids  = [module.ssm_sg.security_group_id]
+  subnet_ids          = [module.network.private_subnets[0], module.network.private_subnets[1]]
+
+  tags = {
+    Name = "${var.env}-${each.value.tag_name}",
+  }
+}
+
